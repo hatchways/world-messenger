a8290c22c32b9aea43d1615165570b9faa6560a8
Jacob <jacobhtnguyen@gmail.com>
2019-06-23 17:51:42 -0400

    fixed passport middleware auth for profile routes

--- server/routes/api/profiles.js
+++ server/routes/api/profiles.js
@@ -2,18 +2,17 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const jwt = require('jsonwebtoken');
 require("../../config/passport")(passport);
 
 // Load User model
 const User = require("../../models/User");
 
-//TODO implement passport middleware
-
 // @route GET api/profile
 // @desc get user profile
 // @access Public
-router.get("/profile", (req, res) => {
-    return User.findById(req.body.id)
+router.get("/profile", passport.authenticate('jwt', {session: false}), (req, res) => {
+    return User.findById(req.user.id)
         .then((user) => {
             if(!user) {
                 return res.sendStatus(400);
@@ -25,13 +24,12 @@ router.get("/profile", (req, res) => {
 // @route POST api/profile
 // @desc Register user
 // @access Public
-router.post("/profile", (req, res) => {
-    User.findByIdAndUpdate(req.body.id)
+router.post("/profile", passport.authenticate('jwt', {session: false}), (req, res) => {
+    User.findByIdAndUpdate(req.user.id)
         .then( (user) => {
             if(!user) {
                 return res.status(404).json({ msg: "User not found" });
             }
-            console.log(req.body);
             user.profile.firstName = req.body.firstName;
             user.profile.lastName = req.body.lastName;
             user.save();
