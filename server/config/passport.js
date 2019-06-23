dc1fd44f300523cf84a89e2f31dcef50ec213002
Jacob <jacobhtnguyen@gmail.com>
2019-06-23 16:22:58 -0400

    updated passport to only required user info

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 const keys = require("../config/keys");
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.secretOrKey
+};
 
 module.exports = passport => {
     passport.use(
@@ -14,7 +15,10 @@ module.exports = passport => {
             User.findById(jwt_payload.id)
                 .then(user => {
                     if (user) {
-                        return done(null, user);
+                        return done(null, {
+                            id: user.id,
+                            username: user.username,
+                            });
                     }
                     return done(null, false);
                 })
