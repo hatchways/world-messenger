19460ff28176a42257f7a3a720a47a82b9cc52ed
Jacob <jacobhtnguyen@gmail.com>
2019-06-25 21:42:44 -0400

    created messaging and conversation schemas

---
+++ server/models/Message.js
@@ -0,0 +1,25 @@
+// Pull in our required dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create a Schema to represent a Message, defining fields and types as objects of the Schema
+const MessageSchema = new Schema({
+        conversationId: {
+            type: Schema.Types.ObjectId,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        translated: {}
+    },
+    {
+        timestamps: true // Saves createdAt and updatedAt as dates. createdAt will be our timestamp.
+    });
+
+module.exports = mongoose.model('messages', MessageSchema);
