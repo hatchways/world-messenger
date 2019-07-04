9faf8b447185a3e6a90fea2f1fe91790a1ecb135
Jacob <jacobhtnguyen@gmail.com>
2019-07-03 21:58:39 -0400

    created get all contacts endpoint

--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -10,13 +10,39 @@ const User = require("../../models/User");
 const Contact = require("../../models/Contact");
 
 // @route GET api/contacts/
-// @desc Get all contacts
+// @desc returns an array of object{username, status}
 // @access public
-router.get("/", passport.authenticate('jwt', {session: false}), async (req, res) => {
+router.get("/list", passport.authenticate('jwt', {session: false}), async (req, res) => {
 
-    //TODO implement
-    //return all contacts online
+    await User.findById(req.user.id, (error, userReq) => {
+        if (error) {
+            return console.log(`Error has occurred: ${error}`);
+        }
+        userRequester = userReq;
+    });
 
+    let contactList = [];
+    await Contact.find({requester: userRequester}, {recipient: true, status: true})
+        .then(contacts => {
+
+                contacts.forEach(function (item) {
+                    User.findById(item.recipient.toString(), {username: true})
+                        .then(username => {
+                            var contactstatus = {
+                                username: username.username,
+                                status: item.status
+                            };
+                            contactList.push(contactstatus);
+                            console.log(contactList);
+                        }, function (err) {
+                            console.log(err);
+                        });
+                });
+            // return res.json({
+            //     contacts: contactList
+            // });
+            }
+        );
 });
 
 
@@ -27,13 +53,13 @@ router.post("/request", passport.authenticate('jwt', {session: false}), async (r
 
     //Get requester and recipient
     await User.findById(req.user.id, (error, userReq) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Requester = userReq;
     });
     await User.findOne({email: req.body.email}, (error, userRec) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Recipient = userRec;
@@ -53,13 +79,13 @@ router.post("/request", passport.authenticate('jwt', {session: false}), async (r
 
     await User.findOneAndUpdate(
         {_id: Requester.id},
-        { $push: { contacts: docRequester._id }}
+        {$push: {contacts: docRequester._id}}
     );
     await User.findOneAndUpdate(
         {_id: Recipient.id},
-        { $push: { contacts: docRecipient._id }}
+        {$push: {contacts: docRecipient._id}}
     );
-    return res.status(200).json({ msg: "Request sent"});
+    return res.status(200).json({msg: "Request sent"});
 });
 
 // @route POST api/contacts/accept
@@ -69,27 +95,27 @@ router.post("/accept", passport.authenticate('jwt', {session: false}), async (re
 
     //Get requester and recipient
     await User.findById(req.user.id, (error, userReq) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Requester = userReq;
     });
     await User.findOne({email: req.body.email}, (error, userRec) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Recipient = userRec;
     });
 
     await Contact.findOneAndUpdate(
-        { requester: Requester, recipient: Recipient },
-        { $set: { status: 3 }}
+        {requester: Requester, recipient: Recipient},
+        {$set: {status: 3}}
     );
     await Contact.findOneAndUpdate(
-        { recipient: Requester, requester: Recipient },
-        { $set: { status: 3 }}
+        {recipient: Requester, requester: Recipient},
+        {$set: {status: 3}}
     );
-    return res.status(200).json({ msg: "Concact acccepted"});
+    return res.status(200).json({msg: "Concact acccepted"});
 });
 
 // @route POST api/contacts/reject
@@ -99,31 +125,31 @@ router.post("/reject", passport.authenticate('jwt', {session: false}), async (re
 
     //Get requester and recipient
     await User.findById(req.user.id, (error, userReq) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Requester = userReq;
     });
     await User.findOne({email: req.body.email}, (error, userRec) => {
-        if(error) {
+        if (error) {
             return console.log(`Error has occurred: ${error}`);
         }
         Recipient = userRec;
     });
 
     const docA = await Friend.findOneAndRemove(
-        { requester: Requester, recipient: Recipient }
+        {requester: Requester, recipient: Recipient}
     );
     const docB = await Friend.findOneAndRemove(
-        { recipient: Requester, requester: Recipient }
+        {recipient: Requester, requester: Recipient}
     );
     await User.findOneAndUpdate(
-        { _id: Requester.id },
-        { $pull: { contacts: docA._id }}
+        {_id: Requester.id},
+        {$pull: {contacts: docA._id}}
     );
     await User.findOneAndUpdate(
-        { _id: Recipient.id },
-        { $pull: { contacts: docB._id }}
+        {_id: Recipient.id},
+        {$pull: {contacts: docB._id}}
     )
 
 });
