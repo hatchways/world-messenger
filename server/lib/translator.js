b9301791a7c2d2675447a79ccd35acdf90d8b8e4
Abbas <30557675+AbbasKDG@users.noreply.github.com>
2019-07-05 14:48:40 -0600

    translation-working

--- server/lib/translator.js
+++ server/lib/translator.js
@@ -1,18 +1,30 @@
+// text sent here,
+// 1. tranlated to
+// 2. target language - from user database
+// 3. output
+
 // Allows to access env variables
 const dotenv = require("dotenv").config();
 console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 async function quickStart(
+  // >> TOD0 - import from config
   projectId = "chatapp-1562205074869" // Your GCP Project Id
 ) {
   // Imports the Google Cloud client library
   const { Translate } = require("@google-cloud/translate");
 
   // Instantiates a client
-  const translate = new Translate({ projectId });
+  const translate = new Translate(
+    // >> json from config
+    {
+      project_id: "chatapp-1562205074869",
+      key: "AIzaSyB1uXN8xUHZrdHhRm6hxIG0zLYHuZGDDxk"
+    }
+  );
 
   // The text to translate
-  const text = "Hello, world!";
+  const text = "My name is Abbas";
 
   // The target language, we choose Russian as an example
   const target = "ru";
