c8a724f835311b7637a420f5358707e0747a4f18
Abbas <30557675+AbbasKDG@users.noreply.github.com>
2019-07-08 00:21:51 -0600

    working

--- server/lib/translator.js
+++ server/lib/translator.js
@@ -1,29 +1,37 @@
 // Allows to access env variables
-const dotenv = require("dotenv").config();
-console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// const dotenv = require("dotenv").config(); // check if this has
 
-async function quickStart(
-  projectId = "chatapp-1562205074869" // Your GCP Project Id
-) {
+// Google project_id for google translate api
+const projectId = "chatapp-1562205074869";
+// Google api key for google translate api
+const key = "AIzaSyB1uXN8xUHZrdHhRm6hxIG0zLYHuZGDDxk";
+// The text to translate
+const text = "Hello, world!";
+// The target language, we choose Russian as an example
+// language documentation: https://cloud.google.com/translate/docs/languages
+const target = "ru";
+
+async function quickStart(projectId, key, target, text) {
   // Imports the Google Cloud client library
   const { Translate } = require("@google-cloud/translate");
 
   // Instantiates a client
-  const translate = new Translate({ projectId });
+  const translate = new Translate({
+    projectId,
+    key
+  });
 
-  // The text to translate
-  const text = "Hello, world!";
-
-  // The target language, we choose Russian as an example
-  const target = "ru";
-
-  // Translates some text into Russian
   const [translation] = await translate.translate(text, target);
 
+  //Print text
   console.log(`Text: ${text}`);
-
+  //Print translation
   console.log(`Translation: ${translation}`);
 }
 
 //Now remember to run this function
-quickStart();
+quickStart(projectId, key, target, text);
+
+module.exports = quickStart;
+
+// quickStart=
