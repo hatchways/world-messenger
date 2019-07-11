52441375e9d5af44611412d6d32a23ef0b668364
Jacob <jacobhtnguyen@gmail.com>
2019-07-11 19:14:21 -0400

    updated conversations to get only conversation id and username updated creating new conversations based on username instead of id

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -18,30 +18,13 @@ router.get("/", requireAuth, (req, res) => {
     // Only return one message from each conversation to display as snippet
     Conversation.find({ participants: req.user.id })
         .select('_id')
+        .populate('participants', 'username')
         .exec(function(err, conversations) {
             if (err) {
                 res.send({ error: err });
                 return next(err);
             }
-
-            // Set up empty array to hold conversations + most recent message
-            let fullConversations = [];
-            conversations.forEach(function(conversation) {
-                Message.find({ 'conversationId': conversation._id })
-                    .sort('-createdAt')
-                    .limit(1)
-                    .populate('author', 'username')
-                    .exec(function(err, message) {
-                        if (err) {
-                            res.send({ error: err });
-                            return next(err);
-                        }
-                        fullConversations.push(message);
-                        if(fullConversations.length === conversations.length) {
-                            return res.status(200).json({ conversations: fullConversations });
-                        }
-                    });
-            });
+            return res.status(200).json({conversations: conversations});
         });
 });
 
@@ -98,28 +81,35 @@ router.post('/new/', requireAuth, function (req, res, next) {
         res.status(422).send({ error: 'Please enter a message.' });
         return next();
     }
-    console.log(req.user.id);
-    const conversation = new Conversation({
-        participants: [req.user.id, req.body.recipient]
-    });
-    conversation.save((err, newConversation) => {
-        if (err) {
-            res.send({ error: err });
-            return next(err);
+
+    User.findOne({username: req.body.recipient}, (error, userRec) => {
+        if (error) {
+            return console.log(`Error has occurred: ${error}`);
         }
-        const message = new Message({
-            conversationId: newConversation._id,
-            body: req.body.composedMessage,
-            author: req.user.id
+        const conversation = new Conversation({
+            participants: [req.user.id, userRec.id]
         });
-        message.save((err, newMessage) => {
+        conversation.save((err, newConversation) => {
             if (err) {
                 res.send({ error: err });
                 return next(err);
             }
-            return res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
+            const message = new Message({
+                conversationId: newConversation._id,
+                body: req.body.composedMessage,
+                author: req.user.id
+            });
+            message.save((err, newMessage) => {
+                if (err) {
+                    res.send({ error: err });
+                    return next(err);
+                }
+                return res.status(200).json({ message: 'Conversation started!', conversationId: conversation._id });
+            });
         });
     });
+
+
 });
 
 module.exports = router;
